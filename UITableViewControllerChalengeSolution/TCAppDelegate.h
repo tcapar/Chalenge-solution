49d1e7fa1120f6e59da48698c5ae4736e2490820
Tarik Capar <Tarik@Tariks-iMac.local>
2014-09-03 18:18:44 +0200

    Initial Commit

---
+++ UITableViewControllerChalengeSolution/TCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TCAppDelegate.h
+//  UITableViewControllerChalengeSolution
+//
+//  Created by Tarik Capar on 03/09/14.
+//  Copyright (c) 2014 Tarik Capar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewControllerChalengeSolution/TCViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCViewController.h
+//  UITableViewControllerChalengeSolution
+//
+//  Created by Tarik Capar on 03/09/14.
+//  Copyright (c) 2014 Tarik Capar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCViewController : UIViewController
+
+@end
